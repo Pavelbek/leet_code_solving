8cba70184dfec80e1bd28892d3cb91a5b95c57c4
Pavlo Argunov <pavlo.argunov@dataart.com>
2023-10-05 14:41:34 +0200

    problem 69 sqrt is solved

---
+++ src/pargunov/easy/Sqrt_x_69.java
@@ -0,0 +1,27 @@
+package pargunov.easy;
+
+public class Sqrt_x_69 {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147483647));
+    }
+
+    private static int mySqrt(int x) {
+        long l = 0;
+        long r = x;
+        long res = 0;
+
+        while(l <= r) {
+            long m = l + (r - l) / 2;
+            if(m * m > x) {
+                r = m - 1;
+            } else if(m * m < x) {
+                l = m + 1;
+                res = m;
+            } else {
+                return (int) m;
+            }
+        }
+
+        return (int) res;
+    }
+}
