2b0176c374c47cc55d9f8d7da7aa036ecfee930c
pargunov <paularg1807@gmail.com>
2023-10-09 11:43:38 +0200

    119 pascal's triangle 2

---
+++ src/pargunov/easy/PascalsTriangle2_119.java
@@ -0,0 +1,22 @@
+package pargunov.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle2_119 {
+    public static void main(String[] args) {
+        System.out.println(getRow(4));
+    }
+    private static List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = 0; i <= rowIndex; i++) {
+            res.add(1);
+            for(int j = i - 1; j > 0; j--) {
+                res.set(j, res.get(j - 1) + res.get(j));
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/pargunov/other/MyArrayList.java
@@ -0,0 +1,79 @@
+package pargunov.other;
+
+import java.util.Objects;
+
+public class MyArrayList<T> implements List<T> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private Object[] arr;
+    private int size;
+
+    public MyArrayList(int initCapacity) {
+        if (initCapacity < 0) {
+            throw new IllegalArgumentException();
+        }
+        arr = new Object[initCapacity];
+    }
+
+    public MyArrayList() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    @Override
+    public void add(T element) {
+        resizeIfNeeded();
+        arr[size++] = element;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        Objects.checkIndex(index, size);
+        resizeIfNeeded();
+        System.arraycopy(arr, index, arr, index + 1, size - index);
+        arr[index] = element;
+    }
+
+    @Override
+    public void set(int index, T element) {
+
+    }
+
+    @Override
+    @SuppressWarnings("Unchecked cast")
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return (T) arr[index];
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    private void resizeIfNeeded() {
+        if (size == arr.length) {
+            Object[] newArr = new Object[arr.length * 2];
+            System.arraycopy(arr, 0, newArr, 0, size);
+            arr = newArr;
+        }
+    }
+}
