388092b8f2491f548432962afa3c563a103068e8
pargunov <paularg1807@gmail.com>
2023-10-13 16:45:30 +0200

    add two numbers 2

--- src/pargunov/easy/AddBinary_67.java
+++ src/pargunov/easy/AddBinary_67.java
@@ -3,6 +3,15 @@ package pargunov.easy;
 public class AddBinary_67 {
     public static void main(String[] args) {
         System.out.println(addBinary("11", "1"));
+        // binary addition logic:
+        // 1 0 0 1
+        // 1 0 1 0
+        // 0 0 1 1
+  // carry 1 0 0 0
+        // sum of 11 + 1
+        //  11
+        //   1
+        // 100
     }
 
     private static String addBinary(String a, String b) {
@@ -15,8 +24,8 @@ public class AddBinary_67 {
             var sum = carry;
             if(i >= 0) sum += a.charAt(i) - '0';
             if(j >= 0) sum += b.charAt(j) - '0';
-            sb.append(sum % 2);
-            carry = sum / 2;
+            sb.append(sum % 2); // 0 % 2 = 0; 1 % 2 = 1; 2 % 2 = 0; 3 % 2 = 1;
+            carry = sum / 2; // 0 / 2 = 0; 1 / 2 = 0; 2 / 2 = 1;
             j--;
             i--;
         }
---
+++ src/pargunov/medium/AddTwoNumbers_2.java
@@ -0,0 +1,47 @@
+package pargunov.medium;
+
+import pargunov.easy.ListNode;
+
+public class AddTwoNumbers_2 {
+    public static void main(String[] args) {
+        // for int < 10 : int / 10 = 0;   int >= 10 = 1;
+        // for int < 10 : int % 10 = int; int > 10 = int - 10; int == 10 = 0;
+
+        //case:
+        // 99
+        // 9
+        // 8 0 1
+        //case:
+        // 9 9 8
+        // 9
+        // 8 0 9
+    }
+    private static ListNode<Integer> addTwoNumbers(ListNode<Integer> l1, ListNode<Integer> l2) {
+        ListNode<Integer> dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while(l1 != null || l2 != null) {
+            int sum = 0 + carry;
+
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+            current.next = new ListNode(sum);
+            current = current.next;
+        }
+
+        if(carry == 1) current.next = new ListNode(carry);
+
+        return dummy.next;
+    }
+}
